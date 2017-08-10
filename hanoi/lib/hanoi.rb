3b72d97b3f1bbb882cd18cc6c69bfe9af4888e9f
Logan Cooper <ldtcoop@gmail.com>
2017-08-09 22:12:29 -0700

    Add #render

--- hanoi/lib/hanoi.rb
+++ hanoi/lib/hanoi.rb
@@ -1,3 +1,21 @@
 class TowersOfHanoiGame
+  attr_reader :stacks
 
+  def initialize(stacks = [[3, 2, 1], [], []])
+    @stacks = stacks
+  end
+
+  def render
+    @stacks.map.with_index do |tower, ind|
+      "Tower #{ind}:  #{tower.join("  ")}"
+    end.join("\n") + "\n"
+  end
+
+  def move(from, to)
+    
+  end
+
+  def game_won?
+
+  end
 end
