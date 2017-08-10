971c10aad3bb1036cc59d0e885a64687e452f84f
Logan Cooper <ldtcoop@gmail.com>
2017-08-09 22:38:45 -0700

    Add Towers#won?

--- hanoi/lib/hanoi.rb
+++ hanoi/lib/hanoi.rb
@@ -1,5 +1,6 @@
 class TowersOfHanoiGame
-  attr_reader :stacks
+
+  attr_reader :stacks, :be_won
 
   def initialize(stacks = [[3, 2, 1], [], []])
     @stacks = stacks
@@ -12,10 +13,18 @@ class TowersOfHanoiGame
   end
 
   def move(from, to)
-    
+    if @stacks[from].empty?
+      raise("cannot move from empty stack")
+    elsif @stacks[to].empty? || @stacks[to].last > @stacks[from].last
+      @stacks[to] << @stacks[from].pop
+    elsif @stacks[to].last < @stacks[from].last
+      raise("cannot move onto smaller disk")
+    end
   end
 
-  def game_won?
-
+  def won?
+    if @stacks[1].length == 3 || @stacks[2].length == 3
+      true
+    end
   end
 end
