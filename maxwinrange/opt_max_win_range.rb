7d3db30db26a8764e8748e9b67b2ea9748d6e6c5
Logan Cooper <ldtcoop@gmail.com>
2017-08-11 08:56:10 -0700

    Add skeleton for optimized windowed max range

---
+++ maxwinrange/opt_max_win_range.rb
@@ -0,0 +1,9 @@
+def windowed_max_range(arr, target)
+  
+end
+
+# tests
+p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
+p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
+p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
+p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
