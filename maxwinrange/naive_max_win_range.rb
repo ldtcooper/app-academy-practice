762e45c7698c0ee03790d1f06c8a2c4a945f75a4
Logan Cooper <ldtcoop@gmail.com>
2017-08-11 08:54:50 -0700

    Add naive windowed maxiumum range

---
+++ maxwinrange/naive_max_win_range.rb
@@ -0,0 +1,17 @@
+def windowed_max_range(arr, win)
+  arr.each_cons(win).inject(0) do |acc, el|
+    rng = el.max - el.min
+    if rng > acc
+      rng
+    else
+      acc
+    end
+  end
+end
+
+
+# tests
+p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
+p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
+p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
+p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
