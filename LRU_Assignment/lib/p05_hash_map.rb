90b9c7467e831e359215faefa11e31e901fe4cf7
Logan Cooper <ldtcoop@gmail.com>
2017-08-13 21:49:52 -0700

    Combine hash set and linked list into hashmap

--- LRU_Assignment/lib/p05_hash_map.rb
+++ LRU_Assignment/lib/p05_hash_map.rb
@@ -1,8 +1,10 @@
 require_relative 'p02_hashing'
 require_relative 'p04_linked_list'
-
+require "byebug"
 class HashMap
   attr_reader :count
+
+  include Enumerable
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
@@ -10,27 +12,45 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    if self.include?(key)
+      bucket(key).update(key, val)
+    else
+      resize! if @count == num_buckets
+      @count += 1
+      bucket(key).append(key, val)
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    if self.include?(key)
+      bucket(key).remove(key)
+      @count -= 1
+    end
   end
 
-  def each
+  def each(&prc)
+    @store.each do |link|
+      link.each do |el|
+        prc.call(el.key, el.val)
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -42,9 +62,16 @@ class HashMap
   end
 
   def resize!
+    old_store = @store.dup
+    @store = Array.new(2 * num_buckets) { LinkedList.new }
+    old_store.each do |link|
+      link.each do |el|
+        self.set(el.key, el.val)
+      end
+    end
   end
 
   def bucket(key)
-    # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 end
