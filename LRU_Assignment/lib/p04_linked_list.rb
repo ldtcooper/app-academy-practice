0ada0fd3be6bb5cc492c5cf8c56f85b06d5be42b
Logan Cooper <ldtcoop@gmail.com>
2017-08-13 21:11:08 -0700

    Finish linked list

--- LRU_Assignment/lib/p04_linked_list.rb
+++ LRU_Assignment/lib/p04_linked_list.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class Node
   attr_accessor :key, :val, :next, :prev
 
@@ -13,13 +14,17 @@ class Node
   end
 
   def remove
-    # optional but useful, connects previous link to next link
-    # and removes self from list.
+    @prev.next = @next
+    @next.prev = @prev
   end
 end
 
 class LinkedList
   def initialize
+    @head = Node.new(:head, true)
+    @tail = Node.new(:tail, true)
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -27,31 +32,65 @@ class LinkedList
     nil
   end
 
+  include Enumerable
+
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
+    @tail.prev == @head
   end
 
   def get(key)
+    self.each do |el|
+      return el.val if el.key == key
+    end
+    nil
   end
 
   def include?(key)
+    self.each do |el|
+      return true if el.key == key
+    end
+    false
   end
 
   def append(key, val)
+    new_guy = Node.new(key, val)
+    new_guy.prev = self.last
+    new_guy.next = @tail
+    self.last.next = new_guy
+    @tail.prev = new_guy
   end
 
   def update(key, val)
+    self.each do |el|
+      if el.key == key
+        el.val = val
+        break
+      end
+    end
   end
 
   def remove(key)
+    self.each do |el|
+      el.remove if el.key == key
+    end
   end
 
-  def each
+  def each(&prc)
+    current = self.first
+    until current == @tail
+      prc.call(current)
+      current = current.next
+    end
+    self
   end
 
   # uncomment when you have `each` working and `Enumerable` included
