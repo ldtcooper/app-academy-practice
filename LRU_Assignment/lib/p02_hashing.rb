c98d303e54308a732ff1966442ec81ccf64db8a7
Logan Cooper <ldtcoop@gmail.com>
2017-08-13 19:49:57 -0700

    Create hashing functions

--- LRU_Assignment/lib/p02_hashing.rb
+++ LRU_Assignment/lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    this_ord = 694_847_539
+    self.each_with_index do |el, ind|
+      this_ord ^= (el.hash * ind)
+    end
+    this_ord
   end
 end
 
 class String
   def hash
+    this_ord = 15_485_867
+    self.each_char.with_index do |el, ind|
+      this_ord ^= (el.ord. * ind)
+    end
+    this_ord
   end
 end
 
@@ -16,6 +26,13 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    this_ord = 334_214_459
+    hsh_arr = self.sort_by(&:first)
+    hsh_arr.to_a.each_with_index do |subarr, ind|
+      subarr.each do |el|
+        this_ord ^= (el.hash * ind)
+      end
+    end
+    this_ord
   end
 end
