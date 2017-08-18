4553653bf6671a647c474fea21bdd87228e84fe7
Logan Cooper <ldtcoop@gmail.com>
2017-08-18 11:16:51 -0700

    Define getter and setter macros

--- W3D5/activerecord_lite/lib/00_attr_accessor_object.rb
+++ W3D5/activerecord_lite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,23 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |variable|
+      # getter
+      define_method(variable) do
+        instance_variable_get(variable)
+      end
+
+      #setter
+      define_method("#{variable}=") do |new_val|
+        instance_variable_set(variable, new_val)
+      end
+    end
   end
 end
+
+# def local_var
+#   @local_var
+# end
+#
+# def local_var=(new_val)
+#   @local_var = new_val
+# end
