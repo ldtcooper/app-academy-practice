230fee361e05ee4f085d43c3894a3a3cb31760e5
Logan Cooper <ldtcoop@gmail.com>
2017-08-18 17:00:55 -0700

    Begin Part 03

--- W3D5/activerecord_lite/lib/03_associatable.rb
+++ W3D5/activerecord_lite/lib/03_associatable.rb
@@ -20,7 +20,9 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    options[:primary_key].nil? ? @primary_key = :id : options[:primary_key]
+    options[:foreign_key].nil? ? @foreign_key = "#{name}_id".to_sym : options[:foreign_key]
+    options[:class_name].nil? ? @class_name = name.capitalize : options[:class_name]
   end
 end
 
