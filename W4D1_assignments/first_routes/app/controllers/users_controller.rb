26d2ceb8f5bed5a24fb97b3c89152f76d1e9ce25
Logan Cooper <ldtcoop@gmail.com>
2017-08-21 16:07:18 -0700

    Add CRUD routes to UserController

--- W4D1_assignments/first_routes/app/controllers/users_controller.rb
+++ W4D1_assignments/first_routes/app/controllers/users_controller.rb
@@ -1,3 +1,32 @@
 class UsersController < ApplicationController
-  
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    user = User.new(params[:user].permit(:name, :email))
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update_attributes(params[:user].permit(:name, :email))
+    render json: user
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+    render json: user
+  end
 end
---
+++ W4D1_assignments/first_routes/db/migrate/20170821220615_users_not_null.rb
@@ -0,0 +1,6 @@
+class UsersNotNull < ActiveRecord::Migration[5.1]
+  def change
+    change_column :users, :name, :string, null:false
+    change_column :users, :email, :string, null:false
+  end
+end
