6199e88a52f451fd492cae6a3581546a0589bfc0
Logan Cooper <ldtcoop@gmail.com>
2017-08-21 13:36:51 -0700

    first_routes initial commit

--- SQLZoo/lib/03a_nobel_bonus.rb
+++ SQLZoo/lib/03a_nobel_bonus.rb
@@ -14,5 +14,8 @@ require_relative './sqlzoo.rb'
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
+    SELECT *
+    FROM nobels
+    WHERE yr = 1917
   SQL
 end
