14794ddfb9410faf90f33811f9c6846fb904990b
Logan Cooper <ldtcoop@gmail.com>
2017-08-21 17:36:24 -0700

    Add associations for ArtworkShare

--- W4D1_assignments/first_routes/app/models/artwork_share.rb
+++ W4D1_assignments/first_routes/app/models/artwork_share.rb
@@ -10,4 +10,16 @@
 #
 
 class ArtworkShare < ApplicationRecord
+  validates :artwork_id, uniqueness: { scope: :user, message: "User can only have one artwork of a certain name" }, presence: true
+  validates :viewer_id, presence: true
+
+  belongs_to :artwork,
+  primary_key: :id,
+  foreign_key: :artwork_id,
+  class_name: :Artwork
+
+  belongs_to :viewer,
+  primary_key: :id,
+  foreign_key: :viewer_id,
+  class_name: :User
 end
