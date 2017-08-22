d883101416b811e6092544dcc45423d4f759939b
Logan Cooper <ldtcoop@gmail.com>
2017-08-21 22:00:58 -0700

    Add index, create, and show to artwork controller

--- W4D1_assignments/first_routes/app/controllers/artwork_controller.rb
+++ W4D1_assignments/first_routes/app/controllers/artwork_controller.rb
@@ -1,2 +1,44 @@
 class ArtworkController < ApplicationController
+  def index
+    @artworks = Artwork.all
+    render json: @artworks
+  end
+
+  def create
+    @artwork = Artwork.new(artwork_params)
+    if @artwork.save
+      render json: @artwork
+    else
+      render json: @artwork.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @artwork = Artwork.find_by(artwork_id)
+    if @artwork
+      render json: @artwork
+    else
+      render json: @artwork.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+
+  end
+
+
+  def destroy
+
+  end
+
+  private
+
+  def artwork_params
+    params[:artwork].permit(:title, :image_url, :artist_id)
+  end
+
+  def artwork_id
+    params[:id]
+  end
+
 end
