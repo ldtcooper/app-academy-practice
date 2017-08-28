4ef960fb0b120aea91070bdf974f518c102ec064
Logan Cooper <ldtcoop@gmail.com>
2017-08-28 15:37:36 -0700

    Add validations to User model

--- goals_app/app/models/user.rb
+++ goals_app/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6 }
+  after_initialize :ensure_session_token
+
+
+
 end
--- goals_app/spec/models/user_spec.rb
+++ goals_app/spec/models/user_spec.rb
@@ -1,50 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # validations
-  describe 'validations' do
-    context 'username' do
-      xit { should validate_presence_of(:username) }
-      xit { should validate_uniqueness_of(:username) }
-    end
-
-    context 'password_digest' do
-      xit { should validate_presence_of(:password_digest) }
-    end
-
-    context 'session_token' do
-      xit { should validate_presence_of(:session_token) }
-      xit { should validate_uniqueness_of(:session_token) }
-    end
-
-    context 'password' do
-      xit { should validate_length_of(:password).is_at_least(6) }
-    end
-  end
 
   # methods
   describe 'password encryption' do
-    before (:each) do
-      User.create!(username: "JohnDoe", password: "password")
-      @user = User.find_by(username: "JohnDoe")
-    end
-    xit 'does not save passwords as plain text' do
-      expect(@user.password).to_not be("password")
+
+    User.create(username: "JohnDoe", password: "password")
+    user = User.find_by(username: "JohnDoe")
+
+    it 'does not save passwords as plain text' do
+      expect(user.password).to_not be("password")
     end
 
-    xit "assigns session token" do
-      expect(@user.session_token).to_not be(nil)
+    it "assigns session token" do
+      expect(user.session_token).to_not be(nil)
     end
   end
 
   describe '::find_by_credentials' do
-    xit "finds a user with correct credentials" do
+    it "finds a user with correct credentials" do
       expect(User.find_by_credentials("JohnDoe", "password")).to be(@user)
     end
 
-    xit "returns nil for incorrect credentials" do
+    it "returns nil for incorrect credentials" do
       expect(User.find_by_credentials("djkgfdag", "password")).to be(nil)
       expect(User.find_by_credentials("JohnDoe", "assword")).to be(nil)
     end
   end
+
+  # validations
+  describe 'validations' do
+    # User.create!(username: "JohnDoe", password: "password")
+    context 'username' do
+      it { should validate_presence_of(:username) }
+      it { should validate_uniqueness_of(:username) }
+    end
+
+    context 'password_digest' do
+      it { should validate_presence_of(:password_digest) }
+    end
+
+    context 'session_token' do
+      it { should validate_presence_of(:session_token) }
+      it { should validate_uniqueness_of(:session_token) }
+    end
+
+    context 'password' do
+      it { should validate_length_of(:password).is_at_least(6) }
+    end
+  end
+
 end
