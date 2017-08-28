e70c582c3d505e05a905ee6be5f44d67e71d5f36
Logan Cooper <ldtcoop@gmail.com>
2017-08-28 14:39:43 -0700

    Add Get#new tests to sessions_controller_specs

--- goals_app/spec/controllers/sessions_controller_spec.rb
+++ goals_app/spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe "GET#new" do
+    xit "renders sign in page" do
+      get :new
+      expect(response).to render_template("new")
+      expect(response).to have_http_status(200)
+    end
+  end
+
 
 end
