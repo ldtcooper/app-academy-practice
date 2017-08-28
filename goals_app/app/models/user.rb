78e0e631a34ec7c82f41e83cd22695a7920bcc73
Logan Cooper <ldtcoop@gmail.com>
2017-08-28 15:38:01 -0700

    Add password and session token methods

--- goals_app/app/models/user.rb
+++ goals_app/app/models/user.rb
@@ -5,6 +5,23 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6 }
   after_initialize :ensure_session_token
 
+  attr_reader :password
 
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token
+    self.session_token = User.generate_session_token
+  end
 
 end
