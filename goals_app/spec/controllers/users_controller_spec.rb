1e927aea66a02ec551bd292204b7e298646bbcee
Logan Cooper <ldtcoop@gmail.com>
2017-08-28 16:17:26 -0700

    Enable users controller tests

--- goals_app/spec/controllers/users_controller_spec.rb
+++ goals_app/spec/controllers/users_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   describe "GET#new" do
-    xit "renders the new user page" do
+    it "renders the new user page" do
       get :new
       expect(response).to render_template("new")
       expect(response).to have_http_status(200)
@@ -11,10 +11,10 @@ RSpec.describe UsersController, type: :controller do
 
   describe "POST#create" do
     context "with valid params" do
-      xit "redirects user" do
+      it "redirects user" do
 
       end
-      xit "logs user in" do
+      it "logs user in" do
         get :create, params: { user: { username: "JohnDoe", password: "password" } }
         user = User.find_by(username: "JohnDoe")
         expect(session[:session_token]).to eq(user.session_token)
@@ -22,12 +22,12 @@ RSpec.describe UsersController, type: :controller do
     end
 
     context "with invalid params" do
-      xit "fails if field not filled in" do
+      it "fails if field not filled in" do
         get :create, params: { user: { username: "", password: "password" } }
         expect flash[:errors].to be_present
       end
 
-      xit "fails if password too short" do
+      it "fails if password too short" do
         get :create, params: { user: { username: "JohnDoe", password: "12345" } }
         expect flash[:errors].to be_present
       end
