ef96fa1d98e3dbbc4d33c859407c9bd0aad2ca9e
Logan Cooper <ldtcoop@gmail.com>
2017-09-03 14:53:14 -0700

    Add prompt function

--- JS_Hanoi/hanoi.js
+++ JS_Hanoi/hanoi.js
@@ -1,5 +1,27 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 class Game {
   constructor() {
-    self.pegs = [[3, 2, 1], [], []];
+    this.pegs = [[3, 2, 1], [], []];
+  }
+
+  promptMove(callback) {
+    let fromIdx;
+    let toIdx;
+    console.log(this.pegs);
+    reader.question("From which tower would you like to take?", (res1) => {
+      fromIdx = parseInt(res1, 10);
+      reader.question("Which tower would you like to move to?", (res2) => {
+        toIdx = parseInt(res2, 10);
+        callback(fromIdx, toIdx);
+      });
+    });
   }
 }
+
+var a = new Game();
